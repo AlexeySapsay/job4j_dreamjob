08ee791e5e16152f5aa860b02bdf31a495190e71
AlekseySapsay <pheniksesalex@mail.ru>
2022-01-13 20:58:52 +0300

    2. Scriplet. Динамическая таблица. [#276520]

---
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -0,0 +1,41 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * https://job4j.ru/profile/exercise/82/task-view/443
+ * <p>
+ * изучение работы с сервлетами
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 13.01.2022
+ */
+public class Store {
+
+    private static final Store INST = new Store();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Ivan", "Junior Java Job",
+                LocalDateTime.of(2022, 1, 10, 10, 10, 10, 0)));
+        posts.put(2, new Post(2, "Alex", "Middle Java Job",
+                LocalDateTime.of(2020, 10, 20, 10, 10, 10, 0)));
+        posts.put(3, new Post(3, "Youriy", "Senior Java Job",
+                LocalDateTime.of(2018, 12, 10, 10, 10, 10, 0)));
+    }
+
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}

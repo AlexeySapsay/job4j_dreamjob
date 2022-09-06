5801134ec3988edb6d4274ffea682ae4f2436809
Сапсай Алексей Юрьевич <ASapsay@alfabank.ru>
2022-09-06 11:46:18 +0400

    1. Техническое задание - проект "Работа мечты" [#504836 #339154] проект собрал и запустил

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -1,5 +1,7 @@
 package ru.job4j.dreamjob.controller;
-
+/**
+ * Изучение работы с контрллерами.
+ */
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
